ffa8c5e8dbc5574b1736951984fcd34fb9f7b0df
Kevan Haggins <63670071+simplesapien@users.noreply.github.com>
2022-08-01 11:27:58 -0400

    Add files via upload
    Updated the colors and layout a little bit

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// pokemonType = pokemonObject.types[0].type.name
-// <a href="https://www.vecteezy.com/free-vector/pokemon-type">Pokemon Type Vectors by Vecteezy</a>
-// orangereebok
-
 const pokemon = document.getElementById('pokemon');
 const pokemonNumber = document.getElementById('pokemon-number');
 const pokemonName = document.getElementById('pokemon-name');
@@ -14,12 +10,14 @@ const specialAttack = document.getElementById('special-attack');
 const specialDefense = document.getElementById('special-defense');
 const speed = document.getElementById('speed');
 const pokemonBackground = document.querySelector('.body-design');
-
-
 const button = document.getElementById('randomizer');
 
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
 const getPokemon = async () => {
-    let randomNumber = Math.ceil(Math.random() * 150);
+    let randomNumber = Math.ceil(Math.random() * 400);
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`);
     const pokemonObject = await response.json();
     const pokemonImage = pokemonObject.sprites.other.dream_world.front_default;
@@ -37,15 +35,13 @@ const getPokemon = async () => {
     pokemonName.innerHTML = capitalizeFirstLetter(pokemonObject.name);
     pokemonNumber.innerHTML = `#${pokemonObject.id}`;
     pokemonTypeImg.src = `img/types/${pokemonType}.png`;
-    pokemonTypeImg.alt = `A symbol for a ${pokemonType} pokemon.`;
+    pokemonTypeImg.alt = `A symbol for a ${pokemonType} pokemon. Vector art by: orangereebok @ Vecteezy`;
     pokemonTypeTxt.innerHTML = capitalizeFirstLetter(pokemonType);
     pokemonBackground.style.background = `var(--${pokemonType})`;
 };
 
+// Load first Pokemon
 getPokemon();
 
 randomizer.addEventListener('click', () => getPokemon());
 
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-};
